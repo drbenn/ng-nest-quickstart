a8eff41b1e8bb3cd86905abc013ce90d4f55814e
drbenn <bennett.daniel@gmail.com>
2025-03-08 19:40:36 -0500

    ui-d: existing user and failed login pages daisified

--- ui-d/src/pages/auth/existing-user-page/existing-user-page.component.ts
+++ ui-d/src/pages/auth/existing-user-page/existing-user-page.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, ActivatedRoute } from '@angular/router';
+import { StandardPageWrapperComponent } from '../../../components/standard-page-wrapper/standard-page-wrapper.component';
 
 @Component({
   selector: 'existing-user-page',
-  imports: [],
+  imports: [CommonModule, StandardPageWrapperComponent, RouterLink, RouterLinkActive],
   templateUrl: './existing-user-page.component.html',
   styleUrl: './existing-user-page.component.scss'
 })
-export class ExistingUserPageComponent {
+export class ExistingUserPageComponent implements OnInit {
+  protected email: string | null = null;
+  protected provider: string | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      this.email = params.get('email');
+      this.provider = params.get('provider');
+    });
+  }
 
 }
--- ui-d/src/pages/auth/failed-login-page/failed-login-page.component.ts
+++ ui-d/src/pages/auth/failed-login-page/failed-login-page.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, ActivatedRoute } from '@angular/router';
+import { StandardPageWrapperComponent } from '../../../components/standard-page-wrapper/standard-page-wrapper.component';
 
 @Component({
   selector: 'failed-login-page',
-  imports: [],
+  imports: [CommonModule, StandardPageWrapperComponent, RouterLink, RouterLinkActive],
   templateUrl: './failed-login-page.component.html',
   styleUrl: './failed-login-page.component.scss'
 })
 export class FailedLoginPageComponent {
+  protected email: string | null = null;
+  protected provider: string | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      this.email = params.get('email');
+    });
+  }
 }
