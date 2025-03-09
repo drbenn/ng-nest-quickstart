006e73b21c7065debe363ec58ce09c5efcf633cb
drbenn <bennett.daniel@gmail.com>
2025-03-09 13:36:37 -0400

    ui-ssr removed from project

--- ui-ssr-d/src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-// https://angular.dev/tools/cli/environments
-
-export const environment = {
-  production: false,
-  apiUrl:'http://localhost:3001/api-v1',
-  darkMode: false,
-  posthogApiKey: 'YOUR_POSTHOG_API_KEY',
-  posthogApiHost: 'https://app.posthog.com'
-};
