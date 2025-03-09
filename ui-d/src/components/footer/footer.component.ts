996f6fb2d53b7311f5db95d95a6558a7bcc70adb
drbenn <bennett.daniel@gmail.com>
2025-03-09 11:56:02 -0400

    ui-d: footer improved

--- ui-d/src/components/footer/footer.component.ts
+++ ui-d/src/components/footer/footer.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroSwatchSolid } from '@ng-icons/heroicons/solid';
 
 @Component({
   selector: 'footbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIcon],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
+  providers: [provideIcons({ heroSwatchSolid })],
 })
 export class FooterComponent {
-
+  protected logoIcon = heroSwatchSolid;
 }
