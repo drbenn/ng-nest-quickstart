9193c27dd0ec5ccf2f139798568112463c4ca4f4
drbenn <bennett.daniel@gmail.com>
2025-05-04 14:42:47 -0400

    ui- popover menu now closing on click outside of menu

--- ui/src/app/components/navbar/components/daisy-dropdown-popover/daisy-dropdown-popover.component.ts
+++ ui/src/app/components/navbar/components/daisy-dropdown-popover/daisy-dropdown-popover.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-daisy-dropdown-popover',
@@ -9,10 +9,24 @@ import { Component, Input, OnInit } from '@angular/core';
 export class DaisyDropdownPopoverComponent implements OnInit {
   @Input() summaryClass!: string;
   @Input() position!: string;
-  protected popoverClass: string = 'dropdown dropdown-left';
+  protected popoverClass: string = 'dropdown dropdown-left';  // rewritten in ngOnInit to include position of menu passed from parent
 
+  // locate dropdown button and listen to DOM to close menu on click outside of menu(standar behavior is for menu to close on click of button menu item, but not close on clicking outside of the menu randomly)
+  @ViewChild('detailsDropdown') detailsDropdownRef!: ElementRef<HTMLDetailsElement>;
+
+  // Listen for click events anywhere in the document
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (this.detailsDropdownRef?.nativeElement.hasAttribute('open')) {
+      const clickedInside = this.detailsDropdownRef.nativeElement.contains(event.target as Node);
+      if (!clickedInside) {
+        this.detailsDropdownRef.nativeElement.removeAttribute('open');
+      }
+    }
+  }
+  
   ngOnInit(): void {
-    this.popoverClass = `dropdown cursor-default ${this.popoverClass}`;
+    this.popoverClass = `dropdown cursor-default ${this.position}`;
   }
 
 }
